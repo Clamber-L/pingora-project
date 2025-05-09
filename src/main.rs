f43f0d8bec37351c9346bafa6d370efa0dcc4989
Carlos <61411632+CarlosLees@users.noreply.github.com>
2025-05-09 16:21:10 +0800

    fix:rustls -> openssl

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use anyhow::Result;
 use pingora::prelude::*;
 use pingora_project::SimpleProxy;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
 
 const PROXY_ADDR: &'static str = "0.0.0.0:9000";
 
 fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::registry()
+        .with(Layer::new().with_filter(LevelFilter::TRACE))
+        .init();
+
+    tracing::info!("Starting server on {}", PROXY_ADDR);
 
     let mut server = Server::new(None)?;
     server.bootstrap();
